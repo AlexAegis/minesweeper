e318f390ec40ee524db74d536ee561749f268662
AlexAegis <alexaegis@gmail.com>
2022-10-27 11:29:17 +0200

    chore: update deps, removed chunksplit

--- apps/desktop/vite.config.ts
+++ apps/desktop/vite.config.ts
@@ -1,21 +1,8 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vite';
-import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		svelte(),
-		chunkSplitPlugin({
-			strategy: 'single-vendor',
-			customSplitting: {
-				rxjs: ['rxjs'],
-				'@tinyslice/core': ['@tinyslice/core'],
-				'@tinyslice/devtools-plugin': ['@tinyslice/devtools-plugin'],
-				'@tinyslice/hydration-plugin': ['@tinyslice/hydration-plugin'],
-				'@tinyslice/logger-plugin': ['@tinyslice/logger-plugin'],
-			},
-		}),
-	],
+	plugins: [svelte()],
 	appType: 'spa',
 });
