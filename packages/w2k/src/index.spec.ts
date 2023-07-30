537fdc84cbb4bd699cb6288123cfc8b2de679fc3
AlexAegis <alexaegis@gmail.com>
2023-07-30 08:40:08 +0200

    chore: add minimal test

---
+++ packages/w2k/src/index.spec.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from 'vitest';
+import * as library from './index.js';
+
+describe('library', () => {
+	it('should be defined', () => {
+		expect(library).toBeDefined();
+	});
+});
