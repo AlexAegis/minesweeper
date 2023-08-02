fcce94a52ce3948f4375eedb5d52f4ecb28b2e92
AlexAegis <alexaegis@gmail.com>
2023-08-02 18:45:22 +0200

    chore: updated packages

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,7 @@
-
-
+// managed-by-autotool
 import config from '@alexaegis/autotool-plugin-prettier/prettier-config';
 
 /** @type {import('prettier').Config} */
 export default {
 	...config,
-	plugins: ['prettier-plugin-svelte', 'prettier-plugin-tailwindcss']
 };
